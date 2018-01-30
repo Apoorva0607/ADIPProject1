fa9020b1a1b3b729fd53c369567657dfba2f3c63
Apoorva Pedgaonkar <appy0607@gmail.com>
2018-01-29 17:39:23 -0700

    change

--- input_project1.m
+++ input_project1.m
@@ -1,38 +1,34 @@
 clc;
 close all;
 %%Problem 1:Find preshapes for all the 100 traingles or ccshapes
-
-%A=zeros(144,144,200);
-%A=load('I1');
-[y1 scale]=pre_shape(I1(:,:,:));
-% figure
-% 
-%    for i=1:size(y1,3)
-% %   plot(y1(:,1,i),y1(:,2,i),'.')
-% %   hold on;
-%    imagesc(y1(:,:,i));
-%    pause(0.2);
-%    colormap gray;
-%    end
+I=0;
+A=zeros(64,2,32);
+A=input1(I)
+[y1 scale]=pre_shape(A(:,:,:))
+figure
+  for i=1:size(y1,3)
+  plot(y1(:,1,i),y1(:,2,i),'.')
+  hold on;
+  end
 %%%%%%% Geodesic Plot
-% [gg dis temp1 Y Rot]=procrustes_user(y1(:,:,1),y1(:,:,20));
-% % Find the error
-% err = Y - y1(:,:,20);
-% err = err .* err;
-% err = sum(err(:));
-% rmse = sqrt(err/3)
-% [T Z]=Log_map(y1(:,:,1),y1(:,:,20),temp1,Y);
-% W=Exp_map(y1(:,:,1),Z);
-% figure
-%   for i=0:0.25:1    %%%% shows all the shapes in one plot
-%   W=Exp_map(y1(:,:,1),i*Z);
-%     plot(W(:,1),W(:,2),'o')
-% % plot([W(1,1),W(2,1),W(3,1),W(1,1)],[W(1,2),W(2,2),W(3,2),W(1,2)])
-%   hold on
-%   end
+[gg dis temp1 Y Rot]=procrustes_user(y1(:,:,1),y1(:,:,20));
+% Find the error
+err = Y - y1(:,:,20);
+err = err .* err;
+err = sum(err(:));
+rmse = sqrt(err/3)
+[T Z]=Log_map(y1(:,:,1),y1(:,:,20),temp1,Y);
+W=Exp_map(y1(:,:,1),Z);
+figure
+  for i=0:0.25:1    %%%% shows all the shapes in one plot
+  W=Exp_map(y1(:,:,1),i*Z);
+    plot(W(:,1),W(:,2),'o')
+% plot([W(1,1),W(2,1),W(3,1),W(1,1)],[W(1,2),W(2,2),W(3,2),W(1,2)])
+  hold on
+  end
 %%%%%%%%%
 %%%%%%%%% Frechet mean
-m0=meanshape(I1);
+m0=meanshape(A);
 %%%%%%%%%
 %%%%%%%%% Principal Geodesic Analysis
 [ F,vk1 ,eigenval,m0,pc_shape]=PGA(A);
